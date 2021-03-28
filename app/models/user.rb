c5b5aa51e655bd24305dcdd8c31f9da331b45ed7
chiakiaosima <starsrock5937@gmail.com>
2021-03-29 07:49:36 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nick_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :nick_name, presence: true, length: { maximum: 40}
+  validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birth_date, presence: true
 end
